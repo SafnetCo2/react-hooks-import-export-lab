ee435508eac3a20230920443ab2b1b6c542f2648
nzi19001@byui.edu <nzi19001@byui.edu>
2024-02-21 14:40:51 +0300

    done assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import NavBar from "../components/NavBar.js";
+import Home from "../components/Home.js";
+import About from "../components/About.js"
+
+
 function App() {
   return (
     <div>
